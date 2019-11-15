48771b704b13a6df8f5d2995f8177e3cc328d4b8
ihab <benakchaihab@gmail.com>
2019-11-15 10:52:33 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="">
+    <h1>{{title}}</h1>
+    <p>{{greeting()}}</p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      title:'yourfirst vue file'
+      }
+
+},
+  methods: {
+    greeting:function(){
+      return 'heeyy';
+    }
+    }
+    }
+</script>
+
+
+
+</style>
